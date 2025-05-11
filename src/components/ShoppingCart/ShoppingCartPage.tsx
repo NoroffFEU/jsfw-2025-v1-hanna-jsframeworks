e71391403f8a168fb5faa3dcb3023f3261714384
hannahoeidahl <hanhoi02118@stud.noroff.no>
2025-05-11 17:58:40 +0200

    Setting up shopping Cart

--- src/components/ShoppingCart/ShoppingCartPage.tsx
+++ src/components/ShoppingCart/ShoppingCartPage.tsx
@@ -1,12 +1,78 @@
-import { Typography } from "@mui/material";
-import { PageLayout } from "../PageLayout/PageLayout";
+import {
+  Typography,
+  Card,
+  Box,
+  CardMedia,
+  CardContent,
+  Divider,
+} from "@mui/material";
+import { useCart } from "../CartContext/CartContext";
 
 function ShoppingCart() {
+  const { cartItems } = useCart();
+
   return (
-    <PageLayout>
-      <Typography variant="h4">Your cart is empty</Typography>
-    </PageLayout>
+    <>
+      {cartItems.map(({ product, quantity }) => (
+        <Card
+          key={product.id}
+          sx={{
+            maxWidth: { xs: 300, sm: "100%", md: 400 },
+            margin: "auto",
+            mt: 4,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: { xs: "column", md: "row" },
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                width: 300,
+                height: 300,
+                objectFit: "cover",
+                margin: "auto",
+              }}
+              image={product.image.url}
+              alt={product.image.alt}
+            />
+            <CardContent sx={{ flex: 1 }}>
+              <Typography variant="h5">{product.title}</Typography>
+
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Quantity: {quantity}
+              </Typography>
+
+              <Typography variant="h6" sx={{ mb: 2 }}>
+                {product.discountedPrice ? (
+                  <>
+                    <span
+                      style={{
+                        textDecoration: "line-through",
+                        marginRight: "8px",
+                      }}
+                    >
+                      ${product.price}
+                    </span>
+                    <span style={{ color: "#00796b" }}>
+                      ${product.discountedPrice}
+                    </span>
+                  </>
+                ) : (
+                  <span>${product.price}</span>
+                )}
+              </Typography>
+              <Divider sx={{ my: 2 }} />
+            </CardContent>
+          </Box>
+        </Card>
+      ))}
+      ,
+    </>
   );
 }
-
 export { ShoppingCart };
